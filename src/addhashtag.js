755f1afd0f937887251171b46c392e6035c76b50
Tom Becker <tjbecker@me.com>
2019-07-11 15:04:28 +0700

    saving before comp blows up

---
+++ src/addhashtag.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './right-sidebar.css';
+
+
+class Addhashtag extends Component {
+	//data
+	state = {
+		hashtag: this.props.hashtag
+	}
+	//functions
+
+
+
+
+
+	//render
+	render() {
+		return (
+
+			<h5>hello</h5>
+		)
+	}
+}
+
+
+
+export default Addhashtag;
--- src/right-sidebar.js
+++ src/right-sidebar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './right-sidebar.css';
-// import Trends from './trends'
+import Addhashtag from './addhashtag'
 // import axios from 'axios'
 
 class Rightsidebar extends Component {
@@ -9,28 +9,7 @@ class Rightsidebar extends Component {
 		hashtags: []
 	}
 	//functions
-	// componentDidMount() {
-	// 		axios.get(`http://localhost:4000/api/hashtag`).then((res)=> {
-	// 				res.data[0].active = true
-	// 				this.setState({
-	// 					hashtags: res.data
-	// 				})
-	// 		this.selectHashtag(res.data[0]._id)
-	// 		}).catch((err)=> {
-	// 			console.log('err', err);
-	// 		})
-	// 	}
 
-		// selectHashtag = (id) => {
-		// 	let hashtags = this.state.hashtags
-		// 	hashtags.map((c)=> c.active = false)
-		// 	let hashtag = hashtags.find((c) => c._id === id)
-		// 	hashtag.active = true
-		// 	this.setState({hashtags})
-		// 	// console.log(this.state.channels);
-		//
-		// 	this.props.getTweets(id)
-		// }
 
 
 
@@ -44,9 +23,9 @@ class Rightsidebar extends Component {
 					<div className="card" >
 
 						<div className="card-body">
-						<h5 className="card-title">Worldwide Trends</h5>
+						<h5 className="card-title">Add a Hashtag</h5>
 						<ul className="list-unstyled">
-							<li>trend</li>
+							<Addhashtag />
 						</ul>
 					</div>
 
